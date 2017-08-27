502587e9c665e742f00f258305bfac3d2281eb0e
Jimmy <jimmymasaru@gmail.com>
2017-08-27 23:02:59 +1200

    iOS Font Profile Generator 1.0.0

---
+++ Formula/fp-gen.rb
@@ -0,0 +1,24 @@
+class FpGen < Formula
+  desc "iOS Font Profile Generator"
+  homepage "https://github.com/jimmymasaru/fp-gen/"
+  url "https://github.com/jimmymasaru/fp-gen.git",
+    :tag => "1.0.0"
+  version "1.0.0"
+
+  depends_on "node" => :run
+
+  def install
+    system "/usr/local/bin/npm", "install"
+    ohai "Installing components ..."
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"fpgen"
+  end
+
+  test do
+    assert File.file?("#{bin}/fpgen")
+  end
+
+  def post_install
+    ohai "Run 'fpgen'."
+  end
+end
