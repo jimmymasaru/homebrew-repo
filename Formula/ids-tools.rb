db3baa268a745a5ab81604eb8c4077b64250d3a0
Jimmy Page <12769660+jimmymasaru@users.noreply.github.com>
2021-06-20 13:15:39 +1200

    Update ids-tools formula

--- Formula/ids-tools.rb
+++ Formula/ids-tools.rb
@@ -1,12 +1,15 @@
+require "language/node"
+
 class IdsTools < Formula
   desc "IDS (Ideographic Description Sequence) Tools"
   homepage "https://github.com/jimmymasaru/ids-tools/"
   url "https://github.com/jimmymasaru/ids-tools.git",
     :tag => "1.1.0"
   version "1.1.0"
+  depends_on "node"
 
   def install
-    system "/usr/local/bin/npm", "install"
+    system "npm", "install", *Language::Node.local_npm_install_args
     opoo "Initializing local IDS database. This could take a few minutes."
     system "./idsdictgen", "data/ids/ids-cdp.txt", "--char", "data/ids-cdp-dict.txt", "--comp", "data/ids-cdp-comp.txt"
     libexec.install Dir["*"]
@@ -18,6 +21,6 @@ class IdsTools < Formula
   end
 
   def post_install
-    ohai "Run 'idsquery'."
+    ohai "Run 'idsquery' to start using."
   end
 end
